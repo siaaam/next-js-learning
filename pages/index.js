ca8f024222efdce96d9873a8bb5abdc8a6b8b516
siam666-shell <abdullahsiam009@gmail.com>
2022-08-24 14:30:13 +0600

    client side and programatically routing and 404 page defined

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 function Home() {
-  return <h1>Home Page</h1>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Placing Your order');
+    router.push('/product');
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <br />
+      <Link href="/product">
+        <a>Product</a>
+      </Link>
+      <br />
+
+      <button onClick={handleClick}>Place Order</button>
+    </div>
+  );
 }
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,35 @@
-function ProductList() {
+import Link from 'next/link';
+
+function ProductList({ productId = 100 }) {
   return (
     <>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+
+      <h1>
+        <Link href="/product/1">
+          <a>Product1 </a>
+        </Link>
+      </h1>
+
+      <h1>
+        <Link href="/product/2">
+          <a>Product 2</a>
+        </Link>
+      </h1>
+
+      <h1>
+        <Link href="/product/3" replace>
+          <a>Product 3</a>
+        </Link>
+      </h1>
+
+      <h1>
+        <Link href={`/product/${productId}`}>
+          <a>Product {productId}</a>
+        </Link>
+      </h1>
     </>
   );
 }
